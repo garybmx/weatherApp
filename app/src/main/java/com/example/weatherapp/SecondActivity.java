69b7e6b2fc925b920103676e311266b2f349f594
Igor U <garymtb@yandex.ru>
2019-04-28 21:28:39 +0300

    3. * Добавьте на первый экран, помимо поля для города, дополнительные параметры (влажность, скорость ветра, давление и тд) в виде чекбоксов или свитчей. Передавайте их значения на второй экран и выводите дополнительную информацию в зависимости от значений.

---
+++ app/src/main/java/com/example/weatherapp/SecondActivity.java
@@ -0,0 +1,56 @@
+package com.example.weatherapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Date;
+
+public class SecondActivity extends AppCompatActivity {
+    TextView cityText;
+    TextView textDate;
+    TextView textWindCourse;
+    TextView textWindSpeed;
+    LinearLayout dampBlock;
+    LinearLayout windBlock;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        cityText = findViewById(R.id.city_text);
+        textDate = findViewById(R.id.date_text);
+        textWindCourse = findViewById(R.id.wind_course_text);
+        textWindSpeed = findViewById(R.id.wind_value_text);
+        dampBlock = findViewById(R.id.damp_block);
+        windBlock = findViewById(R.id.wind_block);
+
+        if(getIntent().hasExtra(MainActivity.cityExtra)){
+            cityText.setText(getIntent().getStringExtra(MainActivity.cityExtra));
+        }
+        if(getIntent().hasExtra((MainActivity.isDateExtra))){
+            textDate.setVisibility(View.VISIBLE);
+            Date date = new Date();
+            textDate.setText(String.format("%1te %2tB %3tY %4tA", date, date,date, date));
+        }
+        if(getIntent().hasExtra((MainActivity.isHumidityExtra))){
+            dampBlock.setVisibility(View.VISIBLE);
+        }
+
+        if((!getIntent().hasExtra(MainActivity.isWindCourseExtra)) && (!getIntent().hasExtra(MainActivity.isWindSpeedExtra))){
+            windBlock.setVisibility(View.GONE);
+        }
+        if(getIntent().hasExtra(MainActivity.isWindCourseExtra)){
+            textWindCourse.setVisibility(View.VISIBLE);
+        }
+        if(getIntent().hasExtra(MainActivity.isWindSpeedExtra)){
+            textWindSpeed.setVisibility(View.VISIBLE);
+        }
+
+
+
+    }
+}
