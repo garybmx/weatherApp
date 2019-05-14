17b921fe4a62e9229e66b315ae8cfbbed820dd3d
gary <garymtb@yandex.ru>
2019-05-15 00:34:24 +0300

    В погодном приложении создайте заготовку на основе приложения CityInfo. Подготовьте фрагмент с выводом температурных характеристик вместо герба: чтобы выбрав город, можно было посмотреть температуру в нем.

---
+++ app/src/main/java/com/example/weatherapp/Fragment1.java
@@ -0,0 +1,82 @@
+package com.example.weatherapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Fragment1 extends ListFragment {
+
+    boolean isExistFragment2;
+    int currentPosition = 0;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.Cities,
+                android.R.layout.simple_list_item_activated_1);
+        setListAdapter(adapter);
+        View detailsFrame = getActivity().findViewById(R.id.temp_value);
+        isExistFragment2 = detailsFrame != null && detailsFrame.getVisibility() == View.VISIBLE;
+        if (savedInstanceState != null) {
+            currentPosition = savedInstanceState.getInt("CurrentCity", 0);
+        }
+        if (isExistFragment2) {
+            getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+            showFragment2();
+        }
+    }
+    
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("CurrentCity", currentPosition);
+    }
+
+    
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        currentPosition = position;
+        showFragment2();
+    }
+
+    private void showFragment2() {
+        if (isExistFragment2) {
+            getListView().setItemChecked(currentPosition, true);
+            Fragment2 detail = (Fragment2)
+                    getFragmentManager().findFragmentById(R.id.temp_value);
+            if (detail == null || detail.getIndex() != currentPosition) {
+                detail = Fragment2.create(currentPosition);
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.temp_value, detail);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+            }
+        } else {
+            Intent intent = new Intent();
+            intent.setClass(getActivity(), SecondActivity.class);
+            intent.putExtra("index", currentPosition);
+            startActivity(intent);
+        }
+
+    }
+}
--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.weatherapp;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,49 +13,11 @@ import android.widget.EditText;
 import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-    Button sendButton;
-    EditText cityName;
-    Switch isDate;
-    CheckBox isHumidity;
-    CheckBox isWindCourse;
-    CheckBox isWindSpeed;
-    public final static String cityExtra = "cityName";
-    public final static String isDateExtra = "isDate";
-    public final static String isHumidityExtra = "isHumidity";
-    public final static String isWindCourseExtra = "isCourseWind";
-    public final static String isWindSpeedExtra = "isSpeedWind";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        sendButton = findViewById(R.id.send_button);
-        cityName = findViewById(R.id.city_name);
-        isDate = findViewById(R.id.isDate);
-        isHumidity = findViewById(R.id.isDamp);
-        isWindCourse = findViewById(R.id.isWindCourse);
-        isWindSpeed = findViewById(R.id.isWindValue);
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MainActivity.this.startActivity(getActivityIntent());
-            }
-
-            private Intent getActivityIntent() {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra(cityExtra, cityName.getText().toString());
-                if(isDate.isChecked())
-                    intent.putExtra(isDateExtra, isDate.getText().toString());
-                if(isHumidity.isChecked())
-                    intent.putExtra(isHumidityExtra, isHumidity.getText().toString());
-                if(isWindCourse.isChecked())
-                    intent.putExtra(isWindCourseExtra, isWindCourse.getText().toString());
-                if(isWindSpeed.isChecked())
-                    intent.putExtra(isWindSpeedExtra, isWindSpeed.getText().toString());
-                return intent;
-            }
-        });
-
+        setContentView(R.layout.activity_main_layout);
     }
+
 }
