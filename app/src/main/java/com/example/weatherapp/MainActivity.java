bcddc74379e1014bae6088baf0ed173695aef5f7
Igor U <garymtb@yandex.ru>
2019-05-27 09:42:51 +0300

    1. Создать tool bar для приложения с меню. Подумать, какие пункты меню вам нужны. 2. Добавить в проект Navigation Drawer. В Drawer добавить Header с аватаром пользователя, а также несколько пунктов: «О разработчике», «Форма обратной связи» + пункты на ваше усмотрение; 3. *Добавить цветовую индикацию пунктов Drawer, чтобы всегда подсвечивался тот пункт, который выбран.

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,23 +1,114 @@
 package com.example.weatherapp;
 
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.Switch;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    NavigationView navigationView;
+    FragmentManager fragmentManager;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_layout);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+       navigationView = findViewById(R.id.nav_view);
+       navigationView.setNavigationItemSelectedListener(this);
+       fragmentManager = getSupportFragmentManager();
+       Fragment fragment = new Fragment1();
+       fragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit();
+       navigationView.setCheckedItem(R.id.nav_city);
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        Fragment fragment = null;
+        int id = item.getItemId();
+        showToast();
+
+        if (id == R.id.nav_city) {
+            fragment = new Fragment1();
+        } else if (id == R.id.nav_day) {
+            fragment = new Fragment2();
+
+        }
+        else if (id == R.id.nav_three_days) {
+            showToast();
+        }
+        else if (id == R.id.nav_ten_days) {
+
+        }
+        else if (id == R.id.nav_month) {
+
+        }
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void showToast() {
+
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Показать пункт меню!",
+                Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+    }
 }
+
+
